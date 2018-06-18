8a27634092efb8776173807ee1875e6f287e7cc8
Fanatpwnd <arcanis@mail.ru>
2018-06-19 06:40:29 +1200

    Add Migrations

---
+++ database/migrations/2018_06_18_182037_create_user_infos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->string('user');
+            $table->string('mail');
+            $table->string('avatar');
+            $table->boolean('is_banned');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_infos');
+    }
+}
