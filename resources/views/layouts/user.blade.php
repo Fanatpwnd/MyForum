150575c086b12c85a3c588539d46dd69c620c64c
Fanatpwnd <arcanis@mail.ru>
2018-07-03 21:42:33 +1200

    FastFix

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -30,11 +30,10 @@
 <hr>
 <h1>{{ $content['nickname'] }}'s messages</h1>
 @if( count($content->user->messages->where('is_delete', false)) >= 1)
-    @foreach ($content->user->messages->where('is_delete', false)->paginate(5) as $item)
+    @foreach ($content->user->messages->where('is_delete', false) as $item)
             <div style="padding: 10px;border-style: solid;border-width: 1px; margin-top: 2px; margin-left: 30px; margin-right: 30px;">
             <h3><a href='\Messages\{{$item->thread->id}}'>Thread: {{ $item->thread->title }}</a></h3><p class='text-info'>Author thread: <a href='/user/{{$item->thread->user['id']}}'> {{ $item->thread->user->userInfo['nickname'] }}</a></p>
                 <p>Message: {{$item['body']}}</p>
             </div> 
     @endforeach 
-    {{ $content->user->messages->where('is_delete', false)->paginate(5)->links() }}
 @endif
